6a512cec766c802ac62308e984d88bc9634a2fae
Zeiyy || Skizo <81281058+xznsenpai@users.noreply.github.com>
2023-05-27 17:37:50 +0800

    update instagram.js

--- routes/instagram.js
+++ routes/instagram.js
@@ -4,6 +4,13 @@ let { igApi, getCookie } = require("insta-fetcher");
 let { sessions_ig } = require('../config');
 let ig = new igApi(sessions_ig);
 
-
+sh.all('/igdl', async (req, res) => {
+	if (!req.query.url) throw res.json({status: 200, message: "url parameter cannot be empty"})
+	ig.fetchPost(req.query.url)
+	.then((v) => {
+    res.status(200)
+	.json(v);
+	});
+});
 
 module.exports = sh;
