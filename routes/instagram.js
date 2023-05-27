b62c1f331d049b23b6798afac0e13a280e8aa62f
Zeiyy || Skizo <81281058+xznsenpai@users.noreply.github.com>
2023-05-27 17:32:32 +0800

    update instagram.js

--- routes/instagram.js
+++ routes/instagram.js
@@ -2,6 +2,8 @@ let express = require("express");
 let sh = express.Router();
 let { igApi, getCookie } = require("insta-fetcher");
 let { sessions_ig } = require('../config');
-let ig = new igApi("your cookie");
+let ig = new igApi(sessions_ig);
+
+
 
 module.exports = sh;
