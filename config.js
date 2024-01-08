2540dc56b11992ffa0f529a9e943501a33e60beb
Kanvret12 <82053023+Kanvret12@users.noreply.github.com>
2024-01-08 21:57:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     dbURI: 'mongodb+srv://shield:Kelebihan@cluster0.lxds2ta.mongodb.net/api?retryWrites=true&w=majority&ssl=true',
     keyAi: 'Bearer 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',
     email: 'verification@shieldid.site',
-    smtp: 'AwTkEnKKKh3Q',
+    smtp: 'jRb6fMfrrSRC',
     Creator: 'SHIELD',
     sessions_ig: "mid=ZHHMFAALAAEBjPKE-ZdIPDHPXvvf; ig_did=E259D69A-477A-4FAA-AD51-71131CE9B3F4; ig_nrcb=1; datr=EsxxZEmDA3o7FKR8EEThmfni; shbid=15857\05411616657127\0541720371217:01f7f4983b8fe6e144355e439f5f18ed74dce3642c9b0d50f8fd90ac8e7856ae4c227d33; shbts=1688835217\05411616657127\0541720371217:01f7d5c24f93c68ed893f67dcfc347d0a73cde495db42b4d6b2625da83a245aa0ae77633; csrftoken=usnZ1tbJZX8utOyameGUn6qLYMJ1nOXV; ds_user_id=48420725126; sessionid=48420725126%3AZhccDTGDsF04Dw%3A6%3AAYegb-12JSzTIc-PFoRs4IndI6kdCJDu5BEwp7P0UQ; rur=PRN\05448420725126\0541720371780:01f7eb91145ec42b6eb8a25081dd52a55b6ca9429317849430402d13bd7df0c065eda00a"
 };
